1e7ce9c4ea928178908c4cba40981d6f56b3e847
grhhfhdh632-wq <grhhfhdh632@gmail.com>
2026-09-07 19:10:39 +0100

    Update help.js

--- src/commands/Core/help.js
+++ src/commands/Core/help.js
@@ -101,7 +101,7 @@ export async function createInitialHelpMenu(client) {
             },
             {
                 name: '\u200B',
-                value: `-# ${botName} is [open source](https://youtu.be/1jCZX8s3bJE?si=NPOYx-vxVE1I5vJK)`,
+                value: `-# ${botName} is [open source](https://youtube.com/@top_labowi?si=HVHzdR5ORvlWwxJT)`,
                 inline: false,
             },
         ],
